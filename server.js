d8ad6d8a7fac8681b77b6e338c996f8797c6d5a3
megaadmua <adm.mega.ua@gmail.com>
2025-01-16 23:45:48 +0200

    Ваше описание изменений new road pin

--- server.js
+++ server.js
@@ -8,9 +8,9 @@ app.use(express.json());
 
 // Данные с PIN-кодами
 const pins = {
-    "0182": "operator/op1/index.html",
-    "5932": "operator/op2/index.html",
-    "6832": "operator/op3/index.html"
+    "0182": "operator/op1/key013242/index.html",
+    "5932": "operator/op2/key163124/index.html",
+    "6832": "operator/op3/key001381/index.html"
 };
 
 // Обработка запросов проверки PIN
